806dc2cfa135bb08f3f915a4037a3c9a96549ac3
Ruan De Villiers <ruan.devilliers@nintex.com>
2022-01-27 16:17:30 +0200

    updated

--- src/index.tsx
+++ src/index.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import './index.scss';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import History from './components/History/history';
-import reportWebVitals from './reportWebVitals';
-import Game from './components/Game/game';
-import StorageService from './services/storageService';
-import GeoLocationService from './services/GeoLocationService';
-let deviceUniqueId = StorageService.getInstance().getValue('UniqueId', true);
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import "./index.scss";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import History from "./components/History/history";
+import reportWebVitals from "./reportWebVitals";
+import Game from "./components/Game/game";
+import StorageService from "./services/storageService";
+import GeoLocationService from "./services/GeoLocationService";
+let deviceUniqueId = StorageService.getInstance().getValue("UniqueId", true);
 
-console.log('React Version: ' + React.version);
+console.log("React Version: " + React.version);
 deviceUniqueId =
-  deviceUniqueId === null || deviceUniqueId === ''
+  deviceUniqueId === null || deviceUniqueId === ""
     ? (crypto as any).randomUUID()
     : deviceUniqueId;
-StorageService.getInstance().save('UniqueId', deviceUniqueId);
-if ('geolocation' in navigator) {
+StorageService.getInstance().save("UniqueId", deviceUniqueId);
+if ("geolocation" in navigator) {
   GeoLocationService.getInstance(deviceUniqueId);
 } else {
-  console.log('GeoLocation is not supported');
+  console.log("GeoLocation is not supported");
 }
 
 const getVersion = () => {
@@ -32,24 +32,26 @@ ReactDOM.render(
       <nav>
         <Link to="/">Home</Link> | <Link to="newGameForm">New Game</Link>
       </nav>
-      <Routes>
-        <Route path="/" element={<History />} />
-        <Route path="/:latest" element={<History />} />
-        <Route path="game/:gameId" element={<Game />} />
-        <Route path="newGameForm" element={<Game />} />
-      </Routes>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<History />} />
+          <Route path="/:latest" element={<History />} />
+          <Route path="game/:gameId" element={<Game />} />
+          <Route path="newGameForm" element={<Game />} />
+        </Routes>
+      </div>
       <footer>{`Version : ${getVersion()} UniqueID: ${deviceUniqueId}`}</footer>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 const conf = {
   onSuccess: (registration: ServiceWorkerRegistration) => {},
   onUpdate: (reg: ServiceWorkerRegistration) => {
-    reg.waiting!.postMessage({ type: 'SKIP_WAITING' });
-    console.info('New version installed');
-  }
+    reg.waiting!.postMessage({ type: "SKIP_WAITING" });
+    console.info("New version installed");
+  },
 };
 
 serviceWorkerRegistration.register(conf);
